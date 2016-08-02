be43a83db5b85c5f66824034df6c7eb50a060cbc
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-01 20:15:59 -0500

    sum the total score, closes #16

--- Bull's Eye/ViewController.swift
+++ Bull's Eye/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     var currentValue: Int = 0
     var targetValue: Int = 0
+    var score: Int = 0
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
-    
+    @IBOutlet weak var scoreLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewRound()
@@ -28,18 +30,24 @@ class ViewController: UIViewController {
 
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
     }
     
     func startNewRound() {
         targetValue = 1 + Int(arc4random_uniform(100))
 //        currentValue = 50
+        //Sets current value = to existing slider value
         currentValue = lroundf(slider.value)
+        //Sets slider value to the current value
         slider.value = Float(currentValue)
     }
 
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = (100 - difference)
+        score += points
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nYou missed the target by \(difference)" + "\nAnd you have scored \(points) points."
         
         let alert = UIAlertController(title: "Hellow World",
                                       message: message, preferredStyle: .Alert)
@@ -54,6 +62,7 @@ class ViewController: UIViewController {
         startNewRound()
         updateLabels()
     }
+    
     
     @IBAction func sliderMoved(slider: UISlider) {
 //        print("The value of the slider is now: \(slider.value)")
